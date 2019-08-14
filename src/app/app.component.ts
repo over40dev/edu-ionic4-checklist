74981b1b2d75a0eac756e2f121a330e1cdae7f02
cogentx <cogentx@gmail.com>
2019-08-13 22:51:56 -0230

    ionic-capacitor-base app functionality in place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { ChecklistDataService } from './services/checklist-data.service';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+const {SplashScreen, StatusBar} = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -11,17 +11,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private dataService: ChecklistDataService
   ) {
-    this.initializeApp();
+    this.dataService.load();
+
+    SplashScreen.hide().catch((err) => {
+      console.warn(err);
+    });
+
+    StatusBar.hide().catch((err) => {
+      console.warn(err);
+    });
+
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+
 }
